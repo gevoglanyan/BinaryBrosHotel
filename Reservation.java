c11e6fdb48e9234f257ace86a6438fa3deee4646
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2024-02-12 17:09:17 -0800

    Need to add more

--- Reservation.java
+++ Reservation.java
@@ -1,12 +1,20 @@
 //Need to add more to Reservation
 
 public class Reservation {
-    private final userID;
+    private final int userID;
     private String roomType;
 
     public Reservation (int userID, String roomType)
     {
         this.userID = userID;
         this.roomType = roomType;
+    }
+
+    public int getUserId() {
+        return userID;
+    }
+
+    public String getRoomType() {
+        return roomType;
     }
 }
