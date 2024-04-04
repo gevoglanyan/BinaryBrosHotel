9f4d1bc2781a1a94461578c432dfd28815c8fcf1
Diego <148501504+wumbo-jumbo@users.noreply.github.com>
2024-04-03 18:51:04 -0700

    Refactored the Email class

--- src/Objects/Email.java
+++ src/Objects/Email.java
@@ -1,13 +1,12 @@
-import java.util.Properties;
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.mail.Session;
-import javax.activation.*;
+import java.util.*; 
+import javax.mail.*; 
+import javax.mail.internet.*; 
+import javax.activation.*; 
+import javax.mail.Session; 
 import javax.mail.Transport;
 
 /**
-    A way to send emails to customer pertaining to their
+    A way to send emails to customers pertaining to their
     hotel reservation
     @author Binary Bros
     @version 1.0
@@ -29,37 +28,69 @@ public class Email {
     Properties properties = System.getProperties(); 
     properties.setProperty("mail.smtp.host", host); 
     Session session = Session.getDefaultInstance(properties); 
-    /**
-        Represents the message of the email
-     */
-    private String text;
 
- /**
-        Sends email to the customer regarding their hotel reservation
-        @param recipient the customer's email
-        @param text the message in the email
+
+    /**
+        Constructs an email to send to the guest
      */
-  public void sendMessage(String recipient, String text){
-    this.recipient = recipient;
-    this.text = text;
-        try 
-        { 
-            MimeMessage message = new MimeMessage(session); 
-    
-            message.setFrom(new InternetAddress(sender)); 
-    
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
-    
-            message.setSubject("Binary Bros Hotel Reservation"); 
-    
-            message.setText(text); 
-    
-            Transport.send(message); 
-            System.out.println("Email has been sent"); 
-        } 
-        catch (MessagingException mex)  
-        { 
-        mex.printStackTrace(); 
-        } 
+    public Email (String guestEmail) {
+        setRecipient(guestEmail);
     }
+
+    /**
+        Creates default email template
+     * @param header subject line of the email
+     * @param body main message of the email
+     */
+    public void message(String header, String body) {
+       String text = body;
+       String subject = header;
+       try 
+       { 
+           MimeMessage message = new MimeMessage(session); 
+           message.setFrom(new InternetAddress(sender)); 
+           message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+           message.setSubject(subject); 
+           message.setText(text); 
+           Transport.send(message); 
+           System.out.println("Email has been sent"); 
+       } 
+       catch (MessagingException mex)  
+       { 
+       mex.printStackTrace(); 
+       } 
+    }
+        /**
+            Sends email about guest's recently made hotel reservation
+        */
+        public void reservationMessage() {
+            String message = "Your hotel reservation has been made. Thank you for choosing Binary Bros for your hotel experience! To update or cancel your reservation, check the reservation status on the website.";
+            String header = "Binary Bros: Hotel Reservation";
+            message(header, message);
+        }
+
+        /**
+            Sends email about a reservation cancelation
+        */
+        public void cancelMessage() {
+            String message = "Your hotel reservation with the Binary Bros has been cancelled.";
+            String header = "Binary Bros: Reservation Cancelation";
+            message(header, message);
+        }
+
+        /**
+            Sends email about update to a reservation
+        */
+        public void updateMessage() {
+            String message = "Your hotel reservation has been successfully updated! Thank you again for choosing Binary Bros for your hotel experience.";
+            String header = "Binary Bros: Reservation Update";
+            message(header, message);
+        }
+
+        /**
+            Sets the guest's email as the recipient address
+        */
+        public void setRecipient(String guestEmail){
+            recipient = guestEmail;
+        }
 }
