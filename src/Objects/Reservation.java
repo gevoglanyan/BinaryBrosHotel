416d2d66b61626735b9274e10cca3b18c671ef18
Diego <148501504+wumbo-jumbo@users.noreply.github.com>
2024-04-08 18:50:23 -0700

    Added documentation.

--- src/Objects/Reservation.java
+++ src/Objects/Reservation.java
@@ -5,21 +5,48 @@ import java.sql.Statement;
 import java.sql.SQLException;
 
 /**
-    Stores User Reservation Info into Database
+    Stores user reservation info into Database
     @author Binary Bros
     @version 1.0
  */
 
 public class Reservation {
 
+    /**
+        Represents the reservation ID number
+    */
     private final int reservationID;
+    /**
+        Represents the room's ID number
+    */
     private final int roomID;
+    /**
+        Represents the reservation check-in date
+    */
     private final String checkInDate;
+    /**
+        Represents the reservation check-out date
+    */
     private final String checkOutDate;
+    /**
+        Represents the reservation's total price
+    */
     private final double totalPrice;
+    /**
+        Represents the number of rooms
+    */
     private final int numberOfRooms;
+    /**
+        Represents the paymentMethod
+    */
     private final int paymentMethod;
+    /**
+        Represents the number of available rooms
+    */
     private final int availableRooms;
+    /**
+        Represents the room type
+    */
     private final String roomType;
 
     public Reservation (int reservationID, int roomID, String checkInDate, String checkOutDate, double totalPrice, 
