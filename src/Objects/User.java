55851ce51138e53a038463964aacfda9056d6f54
Diego <148501504+wumbo-jumbo@users.noreply.github.com>
2024-04-08 18:45:47 -0700

    Added documenation

--- src/Objects/User.java
+++ src/Objects/User.java
@@ -5,19 +5,46 @@ import java.sql.Statement;
 import java.sql.SQLException;
 
 /**
-    Takes User Info and Stores Info into Database
+    Takes user info and stores it into Database
     @author Binary Bros
     @version 1.0
  */
 
 public class User {
+    /**
+        Represents user's unique ID number
+    */
     private int userID;
+    /**
+        Represents user's full name
+    */
     private String fullName;
+    /**
+        Represents user's username
+    */
     private String username;
+    /**
+        Represents user's password
+    */
     private String password;
+    /**
+        Represents user's email address
+    */
     private String email;
+    /**
+        Represents the user's role
+    */
     private String role;
 
+    /**
+        Adds the user and all of their information into the user database
+        @param userID the userID
+        @param fullname the full name
+        @param username the username
+        @param password the password
+        @param email the email
+        @param role the role
+    */
     public User(int userID, String fullName, String username, String password, String email, String role) throws SQLException {
 
         this.userID = userID;
@@ -37,14 +64,23 @@ public class User {
         myDatabaseStatement.executeQuery(mySQL);
     }
 
+    /**
+        Gets the user's username
+    */
     public String getUsername() {
         return username;
     }
 
+    /**
+        Gets the user's password
+    */
     public String getPassword() {
         return password;
     }
 
+    /**
+        Gets the user's role
+    */
     public String getRole() {
         return role;
     }
